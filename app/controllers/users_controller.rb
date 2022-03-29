ab38f7e5711f662f9267fdc521383cf30e9be103
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-03-29 22:07:26 +0200

    trying routing for profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,40 @@
 class UsersController < ApplicationController
-  #  before_action :set_user, only: [:show]
-
-  def index
-    @users = User.all
-  end
+  # before_action :set_user, only: [:show]
+  # set callbacks and authentifacror! etc etc
+  before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show]
 
   def show
-
+    set_user
+    # my_offers/ my_bookings = @offers.index
   end
 
-  def new
-    @user = User.new
-  end
+  # def index
+  #   @user = User.all
+  # end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to edit_offer(@user)
+  #     redirect_to offer(current_user)
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def destroy
-    @user.destroy
-    redirect_to users_path
-  end
+  # def destroy
+  #   @offer.destroy
+  #   # redirect_to
+  # end
 
   private
 
-  # def set_user
-  #   @user = User.find(params[:id])
+  def set_user
+    @user = User.find_by_id(params[:id])
+  end
+  # def set_offer
+  #   @offer = Offer.find(params[:id])
   # end
 
   # def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  resources :users, only: :show do
+  resources :users, only: [:show] do
     resources :offers, only: [:new, :create]
   end
+
   resources :offers, except: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
