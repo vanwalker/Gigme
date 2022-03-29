d66fe6c7920d6c388c19021c8d5a145204c8eae7
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-03-29 17:34:10 +0200

    user path to offers, user views, nested offers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  resources :offers do
-    resources :bookings
+  resources :users do
+    resources :offers
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
