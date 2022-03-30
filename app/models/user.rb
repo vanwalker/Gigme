9c68b538b1220a1145a164e3ef79e328b691af59
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-03-30 11:51:32 +0200

    application controller edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
-  has_one :offer # has one offer?  il est 22h29... donc on affiche qu'une offre...oh lalala
+  has_one :offer
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
